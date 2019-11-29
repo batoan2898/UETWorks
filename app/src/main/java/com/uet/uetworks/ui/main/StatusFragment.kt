7e80a9a0558187cc45d6c8e0e20fc525397dbd36
phong010198 <phong010198@gmail.com>
2019-11-29 07:43:21 +0700

    Update Status Fragment

--- app/src/main/java/com/uet/uetworks/ui/main/StatusFragment.kt
+++ app/src/main/java/com/uet/uetworks/ui/main/StatusFragment.kt
@@ -6,14 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.uet.uetworks.R
+import com.uet.uetworks.adapter.StatusAdapter
+import com.uet.uetworks.model.Company
+import kotlinx.android.synthetic.main.fragment_status.*
 
 class StatusFragment : Fragment() {
+    private lateinit var adapter: StatusAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_status, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val comp = ArrayList<Company>()
+        comp.add(Company("Công ty TNHH UUU", "WAIT"))
+        comp.add(Company("Công ty TNHH EEE", "SELECTED"))
+        comp.add(Company("Công ty TNHH TTT", "WAIT"))
+        adapter = StatusAdapter(comp)
+        recyclerStatusFragment.adapter = adapter
     }
 
     companion object {
